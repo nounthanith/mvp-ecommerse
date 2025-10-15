4c948ba8b5559b5e9096218bdd26fba273b40b6e
NuonThanith <163116657+nounthanith@users.noreply.github.com>
2025-10-15 21:35:20 +0700

    fix db connecting

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    mongoose.connect(process.env.MONGO_URI).then(() => {
+      console.log(`MongoDB Connected`);
+    }).catch((err) => {
+      console.error("Error connecting to MongoDB:", err.message);
+      process.exit(1);
+    });
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1);
+    console.error("Error connecting to MongoDB:", error.message);
   }
 };
 
 module.exports = connectDB;
-
